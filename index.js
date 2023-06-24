92b26850081f0cef47c7c8271d22a5f1924c691e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-06-24 10:14:31 -0500

    Real dep

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-#!/usr/bin/env node
-console.log("Hi I am Hacksore");
+import picocolors from "picocolors"
+export function greet() {
+  const me = picocolors.bold(picocolors.blue("👋 Hi I am Hacksore"))
+  console.log(me);
+  const site = picocolors.bold(picocolors.white("find more here: https://hacksore.com"));
+  console.log(site);
+}
+
